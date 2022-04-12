f731120e96ec59935fa20635f0bd84a7a4d3dbf7
SeungChanAhn <102018797+SeungChanAhn@users.noreply.github.com>
2022-04-12 13:13:24 +0900

    Add files via upload

--- exchange.cpp
+++ exchange.cpp
@@ -6,23 +6,18 @@
 		const float RATE_CNY = 193.54;
 		const float RATE_GBP = 1605.09;
 		int exchangeChoice;
-		float output = 0;
-		int outputFinal = 0;
 		int inputWon = 0;
 		int returnWon = 0;
-		int exchange10000= outputFinal / 10000;
-		int exchange5000= outputFinal % 10000 / 5000;
-		int exchange1000= outputFinal % 5000 / 1000;
-		int exchange500= outputFinal % 1000 / 500;
-		int exchange200= outputFinal % 1000 / 500;
-		int exchange200_100= outputFinal % 200 / 100;
-		int exchange100= outputFinal % 500 / 100;
-		int exchange50= outputFinal % 100 / 50;
-		int exchange20= outputFinal % 50 / 20;
-		int exchange20_10= outputFinal % 20 / 10;
-		int exchange10= outputFinal % 50 / 10;
-		int exchange5= outputFinal % 10 / 5;
-		int exchange1= outputFinal % 5;
+		int outputUSDFinal = 0;
+		int outputJPYFinal = 0;
+		int outputEURFinal = 0;
+		int outputCNYFinal = 0;
+		int outputGBPFinal = 0;
+		float outputUSD = 0;
+		float outputJPY = 0;
+		float outputEUR = 0;
+		float outputCNY = 0;
+		float outputGBP = 0;
 		
 	while(true) {	
 		printf("***************************************************\n");
@@ -34,53 +29,92 @@
 		
 		// 1. 원화 -> USD로. 
 		if (exchangeChoice == 1) {
-			output = inputWon / RATE_USD;
-			outputFinal = output;
-			returnWon = output * 100;
+			outputUSD = inputWon / RATE_USD;
+			outputUSDFinal = outputUSD;
+			returnWon = outputUSD * 100;
 			returnWon = returnWon % 100;
 			returnWon = returnWon * RATE_USD / 100;
 			returnWon = returnWon / 10 * 10;
-			printf("\n%d 원\t->\t %d 달러 환전 \n(100달러 : %d개, 50달러 : %d개, 20달러 : %d개, 10달러 : %d개, 5달러 : %d개, 1달러 : %d개)\n\n", inputWon, outputFinal, exchange100, exchange50, exchange20, exchange20_10, exchange5, exchange1);
+			int exchangeUSD100 = outputUSDFinal / 100;
+			int exchangeUSD50  = outputUSDFinal % 100 / 50;
+			int exchangeUSD20  = outputUSDFinal % 50 / 20;
+			int exchangeUSD10  = outputUSDFinal % 20 / 10;
+			int exchangeUSD5   = outputUSDFinal % 10 / 5;
+			int exchangeUSD1   = outputUSDFinal % 5;
+			printf("\n%d 원\t->\t %d 달러 환전 \n(100달러 : %d개, 50달러 : %d개, 20달러 : %d개, 10달러 : %d개, 5달러 : %d개, 1달러 : %d개)\n\n", inputWon, outputUSDFinal, 
+				exchangeUSD100, exchangeUSD50, exchangeUSD20, exchangeUSD10, exchangeUSD5, exchangeUSD1);
 		}
 		// 2. 원화 -> JPY로. 
 		else if (exchangeChoice == 2) {
-			output = inputWon / RATE_JPY;
-			outputFinal = output;
-			returnWon = output * 100;
+			outputJPY = inputWon / RATE_JPY;
+			outputJPYFinal = outputJPY;
+			returnWon = outputJPY * 100;
 			returnWon = returnWon % 100;
 			returnWon = returnWon * RATE_JPY / 100;
 			returnWon = returnWon / 10 * 10;
-			printf("\n%d 원\t->\t %d 엔 환전 \n(10000엔 : %d개, 5000엔 : %d개, 1000엔 : %d개, 500엔 : %d개, 100엔 : %d개, 50엔 : %d개, 10엔 : %d개, 5엔 : %d개, 1엔 : %d개)\n\n", inputWon, outputFinal, exchange10000, exchange5000, exchange1000, exchange500, exchange100, exchange50, exchange10, exchange5, exchange1);
+			int exchangeJPY10000 = outputJPYFinal / 10000;
+			int exchangeJPY5000	 = outputJPYFinal % 10000 / 5000;
+			int exchangeJPY1000	 = outputJPYFinal % 5000 / 1000;
+			int exchangeJPY500	 = outputJPYFinal % 1000 / 500;
+			int exchangeJPY100	 = outputJPYFinal % 500 / 100;
+			int exchangeJPY50	 = outputJPYFinal % 100 / 50;
+			int exchangeJPY10	 = outputJPYFinal % 50 / 10;
+			int exchangeJPY5	 = outputJPYFinal % 10 / 5;
+			int exchangeJPY1	 = outputJPYFinal % 5;
+			printf("\n%d 원\t->\t %d 엔 환전 \n(10000엔 : %d개, 5000엔 : %d개, 1000엔 : %d개, 500엔 : %d개, 100엔 : %d개, 50엔 : %d개, 10엔 : %d개, 5엔 : %d개, 1엔 : %d개)\n\n", inputWon, outputJPYFinal, 
+				 exchangeJPY10000, exchangeJPY5000, exchangeJPY1000, exchangeJPY500, exchangeJPY100, exchangeJPY50, exchangeJPY10, exchangeJPY5, exchangeJPY1);
 		}
 		// 3. 원화 -> EUR로. 
 		else if (exchangeChoice == 3) {
-			output = inputWon / RATE_EUR;
-			outputFinal = output;
-			returnWon = output * 100;
+			outputEUR = inputWon / RATE_EUR;
+			outputEURFinal = outputEUR;
+			returnWon = outputEUR * 100;
 			returnWon = returnWon % 100;
 			returnWon = returnWon * RATE_EUR / 100;
 			returnWon = returnWon / 10 * 10;
-			printf("\n%d 원\t->\t %d 유로 환전 \n(500유로 : %d개, 200유로 : %d개, 100유로 : %d개, 50유로 : %d개, 20유로 : %d개, 10유로 : %d개, 5유로 : %d개, 1유로 : %d개)\n\n", inputWon, outputFinal, exchange500, exchange200, exchange200_100, exchange50, exchange20, exchange20_10, exchange5, exchange1);
+			int exchangeEUR500 = outputEURFinal / 500;
+			int exchangeEUR200 = outputEURFinal % 500 / 200;
+			int exchangeEUR100 = outputEURFinal % 200 / 100;
+			int exchangeEUR50  = outputEURFinal % 100 / 50;
+			int exchangeEUR20  = outputEURFinal % 50 / 20;
+			int exchangeEUR10  = outputEURFinal % 20 / 10;
+			int exchangeEUR5   = outputEURFinal % 10 / 5;
+			int exchangeEUR1   = outputEURFinal % 5;
+			printf("\n%d 원\t->\t %d 유로 환전 \n(500유로 : %d개, 200유로 : %d개, 100유로 : %d개, 50유로 : %d개, 20유로 : %d개, 10유로 : %d개, 5유로 : %d개, 1유로 : %d개)\n\n", inputWon, outputEURFinal, 
+				 exchangeEUR500, exchangeEUR200, exchangeEUR100, exchangeEUR50, exchangeEUR20, exchangeEUR10, exchangeEUR5, exchangeEUR1);
 		}
 		// 4. 원화 -> CNY로. 
 		else if (exchangeChoice == 4) {
-			output = inputWon / RATE_CNY;
-			outputFinal = output;
-			returnWon = output * 100;
+			outputCNY = inputWon / RATE_CNY;
+			outputCNYFinal = outputCNY;
+			returnWon = outputCNY * 100;
 			returnWon = returnWon % 100;
 			returnWon = returnWon * RATE_CNY / 100;
 			returnWon = returnWon / 10 * 10;
-			printf("\n%d 원\t->\t %d 위안 환전 \n(50위안 : %d개, 20위안 : %d개, 10위안 : %d개, 5위안 : %d개, 1위안 : %d개)\n\n", inputWon, outputFinal, exchange50, exchange20, exchange20_10, exchange5, exchange1);
+			int exchangeCNY50 =	outputCNYFinal / 50;
+			int exchangeCNY20 =	outputCNYFinal % 50 / 20;
+			int exchangeCNY10 =	outputCNYFinal % 20 / 10;
+			int exchangeCNY5  =	outputCNYFinal % 10 / 5;
+			int exchangeCNY1  =	outputCNYFinal % 5;
+
+			printf("\n%d 원\t->\t %d 위안 환전 \n(50위안 : %d개, 20위안 : %d개, 10위안 : %d개, 5위안 : %d개, 1위안 : %d개)\n\n", inputWon, outputCNYFinal, 
+				 exchangeCNY50, exchangeCNY20, exchangeCNY10, exchangeCNY5, exchangeCNY1);
 		}
 		// 5. 원화 -> GBP로. 
 		else if (exchangeChoice == 5) {
-			output = inputWon / RATE_GBP;
-			outputFinal = output;
-			returnWon = output * 100;
+			outputGBP = inputWon / RATE_GBP;
+			outputGBPFinal = outputGBP;
+			returnWon = outputGBP * 100;
 			returnWon = returnWon % 100;
 			returnWon = returnWon * RATE_GBP / 100;
 			returnWon = returnWon / 10 * 10;
-			printf("\n%d 원\t->\t %d 파운드 환전 \n(50파운드 : %d개, 20파운드 : %d개, 10파운드 : %d개, 5파운드 : %d개, 1파운드 : %d개)\n\n", inputWon, outputFinal, exchange50, exchange20, exchange20_10, exchange5, exchange1);
+			int exchangeGBP50 = outputGBPFinal / 50;
+			int exchangeGBP20 = outputGBPFinal % 50 / 20;
+			int exchangeGBP10 = outputGBPFinal % 20 / 10;
+			int exchangeGBP5  = outputGBPFinal % 10 / 5;
+			int exchangeGBP1  = outputGBPFinal % 5;
+			printf("\n%d 원\t->\t %d 파운드 환전 \n(50파운드 : %d개, 20파운드 : %d개, 10파운드 : %d개, 5파운드 : %d개, 1파운드 : %d개)\n\n", inputWon, outputGBPFinal, 
+				 exchangeGBP50, exchangeGBP20, exchangeGBP10, exchangeGBP5, exchangeGBP1);
 		} else {
 			printf("\n1부터 5까지 입력하세요. 다시 입력하세요.\n\n");
 			continue; 
@@ -94,3 +128,4 @@
 	}
 	return 0;
 }
+
