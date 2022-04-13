97438342ee96aaec0e485d09108d3fff4fad09b1
SeungChanAhn <102018797+SeungChanAhn@users.noreply.github.com>
2022-04-14 08:54:17 +0900

    Add files via upload

--- 달력출력.cpp
+++ 달력출력.cpp
@@ -6,9 +6,9 @@ int main() {
     while (true) {
     int year, month;
     int dayOfWeek; // 요일 : 월 화 수 목 금 토 일 ( 0 1 2 3 4 5 6 ) 
-	int days = 0; // 1900년부터 지난 일 수 저장하는 변수 
-    int yearsum = 0; // 1900년부터 지난 일 수  ( 년 만 ) 
-    int monthsum = 0; // 1900년부터 지난 일 수  ( 년 만 ) 
+	int days = 0; // 1900년부터 지난 일 수 저장하는 변수 , 초기화 
+    int yearsum = 0; // 1900년부터 지난 일 수  ( 년 만 )  , 초기화
+    int monthsum = 0; // 1900년부터 지난 일 수  ( 월 만 )  , 초기화
     int monthdaycount = 0;
         printf("년도 입력 : ");
         scanf("%d", & year);
@@ -19,7 +19,7 @@ int main() {
         printf("월 입력 : ");
         scanf("%d", & month);
 
-
+		// 윤년 일때 366일 + 
         for (int i = 1900; i < year; i++) {
             if ((i % 4 == 0) && !(i % 100 == 0) || (i % 400 == 0)) {
                 yearsum += 366;
@@ -27,7 +27,7 @@ int main() {
                 yearsum += 365;
             }
         }
-
+		// 윤년 일때 3~12월 은 +1 
         if ((year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0)) {
             if (month == 1) {
                 monthsum = 0;
@@ -79,12 +79,13 @@ int main() {
                 monthsum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
             } else if (month == 12) {
                 monthsum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
-            }
+            } 
         }
 
         days = yearsum + monthsum; // 1900년부터 지난 일 수 
-        dayOfWeek = days % 7; // 요일 
-
+        dayOfWeek = days % 7; // 요일 : 월 화 수 목 금 토 일 ( 0 1 2 3 4 5 6 ) 
+		
+		// 달력에 숫자 찍기 
         if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
             monthdaycount = 31;
         } else if (month == 4 || month == 6 || month == 9 || month == 11) {
@@ -99,6 +100,8 @@ int main() {
 
         printf("======================%d년 %d월====================\n", year, month);
         printf("일\t월\t화\t수\t목\t금\t토\n");
+        
+        // 월요일에 시작하는 달 
         if (dayOfWeek == 0) {
             printf("\t%d\t%d\t%d\t%d\t%d\t%d\n", 1, 2, 3, 4, 5, 6);
             for (int indexA = 7; indexA <= monthdaycount; indexA += WIDTH) {
@@ -110,6 +113,7 @@ int main() {
                 }
                 printf("\n");
             }
+        // 화요일에 시작하는 달
         } else if (dayOfWeek == 1) {
             printf("\t\t%d\t%d\t%d\t%d\t%d\n", 1, 2, 3, 4, 5);
             for (int indexA = 6; indexA <= monthdaycount; indexA += WIDTH) {
@@ -121,6 +125,7 @@ int main() {
                 }
                 printf("\n");
             }
+        // 수요일에 시작하는 달
         } else if (dayOfWeek == 2) {
             printf("\t\t\t%d\t%d\t%d\t%d\n", 1, 2, 3, 4);
             for (int indexA = 5; indexA <= monthdaycount; indexA += WIDTH) {
@@ -132,6 +137,7 @@ int main() {
                 }
                 printf("\n");
             }
+        // 목요일에 시작하는 달
         } else if (dayOfWeek == 3) {
             printf("\t\t\t\t%d\t%d\t%d\n", 1, 2, 3);
             for (int indexA = 4; indexA <= monthdaycount; indexA += WIDTH) {
@@ -143,6 +149,7 @@ int main() {
                 }
                 printf("\n");
             }
+        // 금요일에 시작하는 달
         } else if (dayOfWeek == 4) {
             printf("\t\t\t\t\t%d\t%d\n", 1, 2);
             for (int indexA = 3; indexA <= monthdaycount; indexA += WIDTH) {
@@ -154,6 +161,7 @@ int main() {
                 }
                 printf("\n");
             }
+        // 토요일에 시작하는 달
         } else if (dayOfWeek == 5) {
             printf("\t\t\t\t\t\t%d\n", 1);
             for (int indexA = 2; indexA <= monthdaycount; indexA += WIDTH) {
@@ -165,6 +173,7 @@ int main() {
                 }
                 printf("\n");
             }
+        // 일요일에 시작하는 달
         } else if (dayOfWeek == 6) {
             for (int indexA = 1; indexA <= monthdaycount; indexA += WIDTH) {
                 for (int indexB = 0; indexB < 7; indexB++) {
